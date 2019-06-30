0a80bde2ff8e8e807c4ee4b2efb8b1817e45b9db
LucianoVirmes <luciano.virmes18@gmail.com>
2019-06-30 15:40:37 -0300

    adicionado api e menu. O menu utiliza o router do vue para navegar sem recarregar

--- vuejs/alurapic/src/App.vue
+++ vuejs/alurapic/src/App.vue
@@ -1,62 +1,50 @@
 <template>
-    <div>
-       <h1>{{titulo}}</h1>
-       
-       <input type="search" class="filtro" placeholder="filte pelo titulo da imagem" v-on:input="filtro = $event.target.value" />
-       {{filtro}}
-       <ul>
-         <li v-for="(foto, i) of fotosFiltradas" :key="i">
-            <meu-painel :titulo="foto.titulo">
-              <imagem-responsiva :url="foto.url" :titulo="foto.descricao"/>
-            </meu-painel>
-         </li>
-       </ul>
-    </div>
+  <div class="corpo">
+
+    <meu-menu :rotas="routes"/>
+
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
+  </div>
 </template>
 
 <script>
 
-import Painel from './components/shared/Painel.vue';
-import ImagemResponsiva from './components/shared/imagem-responsiva/ImagemResponsiva.vue';
+import { routes } from './routes';
+import Menu from './components/shared/menu/Menu.vue';
 
 export default {
-    data(){
-      return{
-        
-          titulo: "Vue pic",
-          fotos: [],
-          filtro: ""
-      }
-    }, 
-    computed: {
-      fotosFiltradas(){
-        if(this.filtro){
-          let expressao = new RegExp(this.filtro.trim(), 'i');
 
-          return this.fotos.filter(foto => expressao.test(foto.titulo));
-        } else {
-          return this.fotos;
-          }
-      }
-    },
-    components: {
-      'meu-painel': Painel,
-      'imagem-responsiva': ImagemResponsiva
-    },
-    created(){
-      let promise = this.$http.get('http://localhost:3000/v1/fotos');
+  components: {
+    'meu-menu' : Menu
+  },
+  
+  data() {
 
-      promise
-      .then(res =>  res.json())
-      .then(fotos => this.fotos = fotos, err => console.log(err));
+    return {
+
+      routes
     }
   }
-  
+}
 </script>
 
 <style>
-  .filtro {
-    display: block;
-    width: 100%;
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
   }
+
+ .pagina-enter, .pagina-leave-active {
+
+     opacity: 0;
+ }
+
+ .pagina-enter-active, .pagina-leave-active {
+
+     transition: opacity .4s;
+ }
+
 </style>
--- vuejs/alurapic/src/components/shared/imagem-responsiva/ImagemResponsiva.vue
+++ vuejs/alurapic/src/components/shared/imagem-responsiva/ImagemResponsiva.vue
@@ -1,15 +1,19 @@
 <template>
-     <img :src="url" :alt="titulo">
+    <img class="imagem-responsiva" :src="url" :alt="titulo">
 </template>
 
 <script>
 export default {
+
     props: ['url', 'titulo']
 }
 </script>
 
 <style scoped>
-    img{
-        width: 100%;
-    }    
+
+  .imagem-responsiva {
+
+    width: 100%;
+  }
+    
 </style>
